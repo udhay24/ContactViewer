3b2e1d88b991c276656c985aa0b4c9d4e99a4d8e
udhay24 <udhayakumar241998@gmail.com>
2018-05-13 11:55:34 +0530

    commit 1
    part of the commit file which will load the contact initially

--- app/src/main/java/com/example/udhay/contactviewer/BackgroundTasks/ContactAsyncTask.java
+++ app/src/main/java/com/example/udhay/contactviewer/BackgroundTasks/ContactAsyncTask.java
@@ -1,20 +1,38 @@
 package com.example.udhay.contactviewer.BackgroundTasks;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
+import com.example.udhay.contactviewer.MainActivity;
 import com.example.udhay.contactviewer.contact_database.ContactsContract;
 
 import com.example.udhay.contactviewer.contact_database.ContactOpenHelper;
 
 public class ContactAsyncTask extends android.support.v4.content.AsyncTaskLoader<Cursor>{
    private final Context context;
+   SharedPreferences settings;
 
     public ContactAsyncTask(Context context ) {
         super(context);
         this.context = context;
+        settings = context.getSharedPreferences(MainActivity.PREFS_NAME, 0);
 
+    }
+
+    @Override
+    protected void onStartLoading() {
+        super.onStartLoading();
+        if (settings.getBoolean("my_first_time", true)) {
+            //the app is being launched for first time, do something
+
+            new ContactsReload(context).execute();
+            MainActivity.launch ++;
+
+            // record the fact that the app has been started at least once
+            settings.edit().putBoolean("my_first_time", false).commit();
+        }
 
     }
 
